3199318660dda5385a93dc44f1b66ff52f3ce0e5
Nick RB <nichellebunagan@gmail.com>
2022-12-24 03:04:02 +0800

    Automated Testing for BMI Calculator.

---
+++ Automated-testing/bmi-calculatorAutomated.test.js
@@ -0,0 +1,11 @@
+describe (`bmiCalculator`,() {
+test(`Check the  BMI and it should be 17.301038062283737 `, (){
+     const weight1 = 50;
+     const height1 = 1.70 
+     const bmi1= 17.301038062283737;
+     const result = bmiCalculator (weight1,height1 );
+     expect(actualResult).toBe(assert);
+    
+});
+
+});
